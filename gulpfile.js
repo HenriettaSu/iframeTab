7bb79db2f47e1139975daf944b89a91e296a7204
HenriettaSu <zenoduaciosiua@hotmail.com>
2016-07-07 18:02:05 +0800

    add new option

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 var gulp = require('gulp'),
-    gulpif = require('gulp-if'),
     changed = require('gulp-changed'),
     sass = require('gulp-sass'),
     autoPrefixer = require('gulp-autoprefixer'),
@@ -10,19 +9,17 @@ var gulp = require('gulp'),
     pump = require('pump'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
-    browserSync = require('browser-sync').create();
+    bs = require('browser-sync').create();
 
-// file path
-var buildSass = 'build/sass/*.scss';
-var buildSassSrc = 'build/sass/';
-var buildCss = 'build/css/*.css';
-var buildCssSrc = 'build/css';
-var buildJs = 'build/js/*.js';
-var distCss = 'dist/css/*.css';
-var distCssSrc = 'dist/css';
-var distJsSrc = 'dist/js';
+var buildSass = 'build/sass/*.scss',
+    buildSassSrc = 'build/sass/',
+    buildCss = 'build/css/*.css',
+    buildCssSrc = 'build/css',
+    buildJs = 'build/js/*.js',
+    distCssSrc = 'dist/css',
+    distJsSrc = 'dist/js';
 
-//sass to css
+// sass to css
 gulp.task('sass-to-css', function(){
   return gulp.src(buildSass)
       .pipe(changed(buildSass))
@@ -30,19 +27,18 @@ gulp.task('sass-to-css', function(){
         console.error(e.message);
       }))
       .pipe(autoPrefixer({
-        browsers: ['last 99 versions'],
-        cascade: false
+        browsers: ['last 99 versions']
       }))
       .pipe(gulp.dest(buildCssSrc))
       .pipe(concat('iframeTab.css'))
       .pipe(gulp.dest(buildCssSrc));
 });
 
-//css minify
+// css minify
 gulp.task('minify-css', function() {
   return gulp.src(buildCss)
       .pipe(changed(buildCss))
-      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(cleanCSS({ compatibility: 'ie8' }))
       .pipe(rename({ suffix: '.min' }))
       .pipe(gulp.dest(distCssSrc));
 });
@@ -59,31 +55,41 @@ gulp.task('jscompress', function (cb) {
   );
 });
 
-//watch sass
+// watch sass
 gulp.task('watch-sass', function (done) {
   gulp.watch(buildSass, ['sass-to-css'])
       .on('end', done);
 });
-//watch css
+// watch css
 gulp.task('watch-css', function (done) {
   gulp.watch(buildCss, ['minify-css'])
       .on('end', done);
 });
-//watch js
+// watch js
 gulp.task('watch-js', function (done) {
   gulp.watch(buildJs, ['jscompress'])
       .on('end', done);
 });
-//前端开发版
+
+// watch
 gulp.task('watch', ['watch-sass', 'watch-css', 'watch-js']);
+
 // browser-sync
 gulp.task('browser-sync',function() {
-  browserSync.init({
+  bs.init({
     server: {
       baseDir: "./"
-    }
+    },
+    //tunnel: "henriettaSu",
+    //online: true
+    /*
+     * server為靜態服務器，運行此會導致一個頁面被同時打開多個（a和b）的情況下，修改a窗口時，b窗口頁面也會被修改
+     * 需要測試功能的時候，開啟tunnel和online即可
+     * 開啟後會非常卡，建議測試結束後關掉
+    */
   });
-  gulp.watch("./dist/**/*.*").on('change', browserSync.reload);
+  gulp.watch("./dist/**/*.*").on('change', bs.reload);
 });
+
 //gulp
 gulp.task('default',['watch','browser-sync']);
